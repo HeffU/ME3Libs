8dccd3b3a366fbafb1e2bd66d31206e96c6b5fe3
Heff <heff@heff.se>
2015-02-24 22:08:29 +0100

    Added a main lexer test. Exposed that the string and name literal matchers had not been added, fixed this.

--- Tests/LexerTests.cs
+++ Tests/LexerTests.cs
@@ -28,14 +28,35 @@ namespace Tests
         [TestMethod]
         public void TestLexerOutput()
         {
-            var test = @"a.a";
+            var test = "class test <<< \"str\"'name' 0.3 ";
             var lexer = new StringLexer(test);
             var tokens = lexer.LexData();
+            var enumerator = tokens.GetEnumerator();
+            enumerator.MoveNext();
 
-            foreach (var token in tokens)
-            {
-                Console.WriteLine(token);
-            }
+            // Match a keyword
+            Assert.AreEqual(enumerator.Current.Type, TokenType.Class);
+            enumerator.MoveNext();
+            // Match a word
+            Assert.AreEqual(enumerator.Current.Value, "test");
+            enumerator.MoveNext();
+            // Match a delimiter with higher priority
+            Assert.AreEqual(enumerator.Current.Type, TokenType.LeftShift);
+            enumerator.MoveNext();
+            // Match another delimiter
+            Assert.AreEqual(enumerator.Current.Type, TokenType.LessThan);
+            enumerator.MoveNext();
+            // Match a string
+            Assert.AreEqual(enumerator.Current.Value, "str");
+            enumerator.MoveNext();
+            // Match a name
+            Assert.AreEqual(enumerator.Current.Value, "name");
+            enumerator.MoveNext();
+            // Match a number
+            Assert.AreEqual(enumerator.Current.Value, "0.3");
+            enumerator.MoveNext();
+            // Assert that we are at the end of stream
+            Assert.IsFalse(enumerator.MoveNext());
         }
     }
 }
